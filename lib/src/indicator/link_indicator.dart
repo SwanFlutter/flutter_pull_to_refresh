01f44a6360633efabdb2fcaa401f73966b57c3cf
SwanFlutter1993 <swan.dev1993@gmail.com>
2024-11-18 00:31:11 -0800

    first commit

---
+++ lib/src/indicator/link_indicator.dart
@@ -0,0 +1,83 @@
+/*
+ * Author: Jpeng
+ * Email: peng8350@gmail.com
+ * Time:  2019-06-26 13:17
+*/
+import 'package:flutter/widgets.dart';
+import 'package:flutter_pull_to_refresh/flutter_pull_to_refresh.dart';
+import 'package:flutter_pull_to_refresh/src/internals/indicator_wrap.dart';
+
+/// enable header link other header place outside the viewport
+class LinkHeader extends RefreshIndicator {
+  /// the key that widget outside viewport indicator
+  final Key linkKey;
+
+  const LinkHeader({super.key, required this.linkKey, super.height = 0.0, RefreshStyle? super.refreshStyle, super.completeDuration = const Duration(milliseconds: 200)});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LinkHeaderState();
+  }
+}
+
+class _LinkHeaderState extends RefreshIndicatorState<LinkHeader> {
+  @override
+  void resetValue() {
+    ((widget.linkKey as GlobalKey).currentState as RefreshProcessor).resetValue();
+  }
+
+  @override
+  Future<void> endRefresh() {
+    return ((widget.linkKey as GlobalKey).currentState as RefreshProcessor).endRefresh();
+  }
+
+  @override
+  void onModeChange(RefreshStatus? mode) {
+    ((widget.linkKey as GlobalKey).currentState as RefreshProcessor).onModeChange(mode);
+  }
+
+  @override
+  void onOffsetChange(double offset) {
+    ((widget.linkKey as GlobalKey).currentState as RefreshProcessor).onOffsetChange(offset);
+  }
+
+  @override
+  Future<void> readyToRefresh() {
+    return ((widget.linkKey as GlobalKey).currentState as RefreshProcessor).readyToRefresh();
+  }
+
+  @override
+  Widget buildContent(BuildContext context, RefreshStatus? mode) {
+    return Container();
+  }
+}
+
+/// enable footer link other footer place outside the viewport
+class LinkFooter extends LoadIndicator {
+  /// the key that widget outside viewport indicator
+  final Key linkKey;
+
+  const LinkFooter({super.key, required this.linkKey, super.height = 0.0, LoadStyle super.loadStyle = LoadStyle.showAlways});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LinkFooterState();
+  }
+}
+
+class _LinkFooterState extends LoadIndicatorState<LinkFooter> {
+  @override
+  void onModeChange(LoadStatus? mode) {
+    ((widget.linkKey as GlobalKey).currentState as LoadingProcessor).onModeChange(mode);
+  }
+
+  @override
+  void onOffsetChange(double offset) {
+    ((widget.linkKey as GlobalKey).currentState as LoadingProcessor).onOffsetChange(offset);
+  }
+
+  @override
+  Widget buildContent(BuildContext context, LoadStatus? mode) {
+    return Container();
+  }
+}
